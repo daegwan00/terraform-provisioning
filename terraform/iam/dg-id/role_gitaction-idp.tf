c19b87fa4beee7f491e03b9cc5f2d9835f1423f5
daegwan00 <daegwanou@gmail.com>
2025-03-14 15:44:02 +0900

    add github action role and idp

--- terraform/iam/dg-id/role_gitaction-idp.tf
+++ terraform/iam/dg-id/role_gitaction-idp.tf
@@ -1,9 +1,9 @@
-# resource "aws_iam_openid_connect_provider" "gitaction_idp" {
-#   url = "https://token.actions.githubusercontent.com"
+resource "aws_iam_openid_connect_provider" "gitaction_idp" {
+  url = "https://token.actions.githubusercontent.com"
 
-#   client_id_list = [
-#     "sts.amazonaws.com"
-#   ]
+  client_id_list = [   # 깃 허브 인증할때 sts를 통해 인증을 하게 됨
+    "sts.amazonaws.com"
+  ]
 
-#   thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
-# }
+  thumbprint_list = ["ffffffffffffffffffffffffffffffffffffffff"]
+}
